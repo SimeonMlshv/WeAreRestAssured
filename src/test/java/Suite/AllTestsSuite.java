6e501bb38ab1927717319cc3e65a89b7a93551de
JK-telerik <zhaneta.krasteva.a61@learn.telerikacademy.com>
2024-11-22 18:27:30 +0200

    PostCREATION fix

---
+++ src/test/java/Suite/AllTestsSuite.java
@@ -0,0 +1,20 @@
+package Suite;
+
+import com.weare.api.tests.*;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+
+@Suite
+@SelectClasses({
+        RegistrationTest.class,
+        InvalidRegistrationTest.class,
+        LoginTest.class,
+        PostApiTests.class,
+        CreatePostTest.class,
+})
+public class AllTestsSuite {
+
+}
+
+
